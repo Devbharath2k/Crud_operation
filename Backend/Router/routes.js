d5e447fd7bb474dd7a3a70d4583ebaf347227d83
devbharath21k <devbharath21k@gmail.com>
2024-04-11 16:55:50 +0530

    new

--- Backend/Router/routes.js
+++ Backend/Router/routes.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const { createTask, getTask, getingtask,updateTask,deletepost } = require("../TaskControler/control.js");
+const {
+  createTask,
+  getTask,
+  getingtask,
+  updateTask,
+  deletepost,
+} = require("../TaskControler/control.js");
 
 router.post("/api/post", createTask);
 router.get("/api/get", getTask);
 router.get("/api/:id", getingtask);
 router.put("/api/:id", updateTask);
 router.delete("/api/:id", deletepost);
-
-
 
 module.exports = router;
